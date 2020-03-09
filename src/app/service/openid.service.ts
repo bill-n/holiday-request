7053c3d3ee465033e195cb58e35f02fb77595d20
bill-n <teebill15@gmail.com>
2020-03-09 16:35:46 +0000

    fixed conflict

--- src/app/service/openid.service.ts
+++ src/app/service/openid.service.ts
@@ -1,61 +1,69 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Requester } from '../form/requester';
-
+import { HttpClientModule } from '@angular/common/http';
+import { Requester } from "../form/requester";
+import { CookieService } from "ngx-cookie-service";
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class OpenidService {
-  private _tokenRequestUrl = "https://oauth2.googleapis.com/token";
-  private validateTokenUrl = "http://localhost:8080/validate";
-  private addUnavailableEmployeeUrl = "http://localhost:8080/addemployee"
-  private checkEmail = "http://localhost:8080/verifymail/";
-  private getRequestsForEmployeeUrl = "http://localhost:8080/api/v1/request/requester/";
-  private makeRequestLink = "http://localhost:8080/api/v1/request"
+  [x: string]: any;
+  private holidayRequestUrl: string;
+  private _tokenRequestUrl: string;
+  private oidc_url: string;
+  private validateTokenUrl = this.holidayRequestUrl + "validate";
+  private addUnavailableEmployeeUrl = this.holidayRequestUrl + "addemployee";
+  private checkEmail = this.holidayRequestUrl + "verifymail/";
+  private getRequestsForEmployeeUrl =
+    this.holidayRequestUrl + "request/requester/";
+  private makeRequestLink = this.holidayRequestUrl + "request";
 
-
-  constructor(private http: HttpClient) { }
-  postAuthenticationCodForAccessAndIdToken(authenticationCode: string): Observable<any> {
+  constructor(private http: HttpClient, private cookieservice: CookieService) {
+    this.holidayRequestUrl = this.cookieservice.get("backend_url");
+    this._tokenRequestUrl = this.cookieservice.get("tokenurl");
+    this.oidc_url = this.cookieservice.get("oidc");
+  }
+  postAuthenticationCodForAccessAndIdToken(
+    authenticationCode: string
+  ): Observable<any> {
     let headers = new HttpHeaders({
       "Content-Type": "application/x-www-form-urlencoded"
     });
-    let body =
-    "code=" +
-    authenticationCode +
-    "&client_id=859455735473-bgmqqco3q588kgaog0g2k0fmnur5qvf9.apps.googleusercontent.com&client_secret=1ivNoDawR_DQAEaVrrc90op4&redirect_uri=https://holiday-request.herokuapp.com/requester&grant_type=authorization_code&";
-  return this.http.post<any>(this._tokenRequestUrl, body, { headers: headers });
+    let body = "code=" + authenticationCode + this.oidc_url;
+    return this.http.post<any>(this._tokenRequestUrl, body, {
+      headers: headers
+    });
   }
 
-  postValidateTokeId(access_token: string): Observable<any>{
-    let headers = new HttpHeaders(); 
-   let head = headers.append("access-token", access_token);
-  return this.http.post<any>(this.validateTokenUrl, new Object(), {headers: head});
-  }
-  
-  getUserDetails(): Observable<any>{
-return this.http.get<any>(this.validateTokenUrl) 
-}
-
-
-addEmployee(requestBody: any): Observable<any> {
-  let body = JSON.stringify(requestBody);
-  let headers = new HttpHeaders({ "Content-Type": "application/json" });
-
-  return this.http.post<any>(this.addUnavailableEmployeeUrl, body, {
-    headers: headers
-  });
-}
-
-checkEmployeePresence(employeeEmail:String):Observable<any> {
-  return this.http.get(this.checkEmail + employeeEmail);
-}
-
-getAllRequestForEmployee(employee_id: any):Observable<any>{
-  return this.http.get(this.getRequestsForEmployeeUrl + employee_id);
+  postValidateTokeId(access_token: string): Observable<any> {
+    let headers = new HttpHeaders();
+    let head = headers.append("access-token", access_token);
+    return this.http.post<any>(this.validateTokenUrl, new Object(), {
+      headers: head
+    });
   }
 
-makeAholidayRequest(employeInfo: any):Observable<any>{
-  return this.http.post(this.makeRequestLink, employeInfo);
-}
+  getUserDetails(): Observable<any> {
+    return this.http.get<any>(this.validateTokenUrl);
+  }
+
+  addEmployee(requestBody: any): Observable<any> {
+    let body = JSON.stringify(requestBody);
+    let headers = new HttpHeaders({ "Content-Type": "application/json" });
+
+    return this.http.post<any>(this.addUnavailableEmployeeUrl, body, {
+      headers: headers
+    });
+  }
+  checkEmployeePresence(employeeEmail: String): Observable<any> {
+    return this.http.get(this.checkEmail + employeeEmail);
+  }
+
+  getAllRequestForEmployee(employee_id: Number): Observable<any> {
+    return this.http.get(this.getRequestsForEmployeeUrl + employee_id);
+  }
+  makeAholidayRequest(employeInfo: any): Observable<any> {
+    return this.http.post(this.makeRequestLink, employeInfo);
+  }
 }
