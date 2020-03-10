e93f35b90c5680ea669a012877655fbfebbd627a
patriciakasapa <patriciakasapa07@gmail.com>
2020-03-10 14:20:33 +0000

    oidc redirection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { OpenidService } from '../service/openid.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { OpenidService } from "../service/openid.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
   authenticationCode;
   idToken;
   isValid = false;
+  oidc_redirect_path;
 
-  constructor(private openId: OpenidService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private openId: OpenidService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.oidc_redirect_path = this.openId.backend_oidc;
+  }
 
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe(queryParam => {
       this.authenticationCode = queryParam.get("code");
-      this.openId.postAuthenticationCodForAccessAndIdToken(this.authenticationCode).subscribe()
+
+      console.log("********** insidopenIde auth", this.authenticationCode);
+
+      // this.oidService._postAuthCodForAccessAndIdToken(this._authCode);
+      // this.openId.postAuthenticationCodForAccessAndIdToken(this.authenticationCode).subscribe(response => {
+      //   console.log(response)
+      // this.idToken = response.id_token
+      // })
     });
-  } 
+  }
 }
