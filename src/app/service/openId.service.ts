5f38d87c2a2543962e4c7ea77a968e49fa3f08b6
bill-n <teebill15@gmail.com>
2020-03-23 12:12:15 +0000

    removed unterminated ;

--- src/app/service/openId.service.ts
+++ src/app/service/openId.service.ts
@@ -13,8 +13,8 @@ export class OpenIdService {
   private addUnavailableEmployeeUrl;
   private checkEmail;
   private getRequestsForEmployeeUrl;
-  private makeRequestLink ="http://localhost:7070/api/v1/request"
-  ";
+  private makeRequestLink ;
+
 
   redirect_page_to_oidc: string;
 
@@ -94,12 +94,12 @@ export class OpenIdService {
   }
 
   makeAholidayRequest(employeInfo: any): Observable<any> {
-    let headers = new HttpHeaders();
-    let body = JSON.stringify(employeInfo);
-    let head = headers.append("access-token", localStorage.getItem("idToken"));
-    console.log("token | ", localStorage.getItem("idToken"));
-    console.log("header  | ", head);
-    console.log("employeInfo | ", employeInfo);
-    return this.http.post(this.makeRequestLink, body);
+    // let headers = new HttpHeaders();
+    // let body = JSON.stringify(employeInfo);
+    // let head = headers.append("access-token", localStorage.getItem("idToken"));
+    // console.log("token | ", localStorage.getItem("idToken"));
+    // console.log("header  | ", head);
+    // console.log("employeInfo | ", employeInfo);
+    return this.http.post(this.makeRequestLink, employeInfo);
   }
 }
