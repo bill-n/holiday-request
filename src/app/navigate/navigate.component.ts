18deeb5b268573b991476decb022f35db56be113
bill-n <teebill15@gmail.com>
2020-03-30 17:20:02 +0000

    code cleanup

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -23,10 +23,8 @@ export class NavigateComponent implements OnInit {
     this.activatedRoute.queryParamMap.subscribe(queryParam => {
       this.authenticationCode = queryParam.get("code");
       this.oidc_redirect_paths = this.openId.redirect_page_to_oidc;
-
       this.router.navigate(["/"]).then(result => {
         window.location.href = this.oidc_redirect_paths;
-        // "https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=https://holiday-request.herokuapp.com/requester&response_type=code&client_id=859455735473-bgmqqco3q588kgaog0g2k0fmnur5qvf9.apps.googleusercontent.com&hd=turntabl.io&prompt=consent";
       });
     });
   }
