0906716297719796a78288fd6c0d6d07bb6b9769
patriciakasapa <patriciakasapa07@gmail.com>
2020-04-16 09:59:11 +0000

    Heroku failing

--- src/app/approver/approver.component.ts
+++ src/app/approver/approver.component.ts
@@ -13,76 +13,78 @@ export interface PeriodicElement {
 }
 
 // TO TRY MY TABLE WITH STATIC DATA
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   {
-//     position: 1,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, , 2020",
-//     req_status: "PENDING"
-//   },
-//   {
-//     position: 2,
-//     requester_name: "Ali Fuseini",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "PENDING"
-//   },
-//   {
-//     position: 3,
-//     requester_name: "Patricia Serwaa Agyekum K.",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Tuesday, 22nd April, 2020",
-//     req_status: "APPROVE"
-//   },
-//   {
-//     position: 4,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "DECLINE"
-//   },
-//   {
-//     position: 5,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "DECLINE"
-//   },
-//   {
-//     position: 6,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "APPROVE"
-//   },
-//   {
-//     position: 7,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "DECLINE"
-//   },
-//   {
-//     position: 8,
-//     requester_name: "John Papa Kwesi Erbynn",
-//     request_start_date: "Wednesday, 15th, April, 2020",
-//     request_report_date: "Friday, 17th April, 2020",
-//     req_status: "PENDING"
-//   }
-// ];
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, , 2020",
+    req_status: "PENDING"
+  },
+  {
+    position: 2,
+    requester_name: "Ali Fuseini",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "PENDING"
+  },
+  {
+    position: 3,
+    requester_name: "Patricia Serwaa Agyekum K.",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Tuesday, 22nd April, 2020",
+    req_status: "APPROVE"
+  },
+  {
+    position: 4,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "DECLINE"
+  },
+  {
+    position: 5,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "DECLINE"
+  },
+  {
+    position: 6,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "APPROVE"
+  },
+  {
+    position: 7,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "DECLINE"
+  },
+  {
+    position: 8,
+    requester_name: "John Papa Kwesi Erbynn",
+    request_start_date: "Wednesday, 15th, April, 2020",
+    request_report_date: "Friday, 17th April, 2020",
+    req_status: "PENDING"
+  }
+];
 
 @Component({
   selector: "app-approver",
   templateUrl: "./approver.component.html",
   styleUrls: ["./approver.component.css"]
 })
+
 export class ApproverComponent implements OnInit {
   idToken;
   userrequest_start_date: String;
   ELEMENT_DATA: PeriodicElement[];
   approved: boolean;
   declined: boolean;
+
   constructor(
     private openId: OpenIdService,
     private activatedRoute: ActivatedRoute
@@ -95,71 +97,71 @@ export class ApproverComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.activatedRoute.queryParamMap.subscribe(queryParam => {
-      this.openId
-        .postAuthenticationCodeForAccessAndIdToken(queryParam.get("code"))
-        .subscribe(response => {
-          this.idToken = response.id_token;
-          localStorage.setItem("idToken", this.idToken);
-          console.log("response from id token |", this.idToken);
-          this.openId
-            .postValidateTokeId(localStorage.getItem("idToken"))
-            .subscribe(res => {
-              localStorage.setItem("userEmail", res.decoded_token.email);
-              localStorage.setItem(
-                "l_request_start_date",
-                res.decoded_token.family_request_start_date
-              );
-              localStorage.setItem(
-                "f_request_start_date",
-                res.decoded_token.given_request_start_date
-              );
-              this.openId
-                .checkEmployeePresence(res.decoded_token.email)
-                .subscribe(response => {
-                  if (response.response.length == 0) {
-                    let requestData = {
-                      employee_email: localStorage.getItem("userEmail"),
-                      employee_firstrequest_start_date: localStorage.getItem(
-                        "f_request_start_date"
-                      ),
-                      employee_lastrequest_start_date: localStorage.getItem(
-                        "l_request_start_date"
-                      )
-                    };
-                    this.openId
-                      .addEmployee(requestData)
-                      .subscribe(response_ => {
-                        console.log(response_);
-                        this.userrequest_start_date =
-                          localStorage.getItem("f_request_start_date") +
-                          " " +
-                          localStorage.getItem("l_request_start_date");
-                        localStorage.setItem(
-                          "employee_id",
-                          response_.employee_id
-                        );
-                        console.log(localStorage.getItem("employee_id"));
-                      });
-                  } else {
-                    localStorage.setItem(
-                      "employee_id",
-                      response.response[0].employee_id
-                    );
-                    this.userrequest_start_date =
-                      localStorage.getItem("f_request_start_date") +
-                      " " +
-                      localStorage.getItem("l_request_start_date");
-                    this.openId
-                      .getAllRequests(response.response[0].employee_id)
-                      .subscribe(data => {
-                        this.dataSource = new MatTableDataSource(data);
-                      });
-                  }
-                });
-            });
-        });
-    });
+    // this.activatedRoute.queryParamMap.subscribe(queryParam => {
+    //   this.openId
+    //     .postAuthenticationCodeForAccessAndIdToken(queryParam.get("code"))
+    //     .subscribe(response => {
+    //       this.idToken = response.id_token;
+    //       localStorage.setItem("idToken", this.idToken);
+    //       console.log("response from id token |", this.idToken);
+    //       this.openId
+    //         .postValidateTokeId(localStorage.getItem("idToken"))
+    //         .subscribe(res => {
+    //           localStorage.setItem("userEmail", res.decoded_token.email);
+    //           localStorage.setItem(
+    //             "l_request_start_date",
+    //             res.decoded_token.family_request_start_date
+    //           );
+    //           localStorage.setItem(
+    //             "f_request_start_date",
+    //             res.decoded_token.given_request_start_date
+    //           );
+    //           this.openId
+    //             .checkEmployeePresence(res.decoded_token.email)
+    //             .subscribe(response => {
+    //               if (response.response.length == 0) {
+    //                 let requestData = {
+    //                   employee_email: localStorage.getItem("userEmail"),
+    //                   employee_firstrequest_start_date: localStorage.getItem(
+    //                     "f_request_start_date"
+    //                   ),
+    //                   employee_lastrequest_start_date: localStorage.getItem(
+    //                     "l_request_start_date"
+    //                   )
+    //                 };
+    //                 this.openId
+    //                   .addEmployee(requestData)
+    //                   .subscribe(response_ => {
+    //                     console.log(response_);
+    //                     this.userrequest_start_date =
+    //                       localStorage.getItem("f_request_start_date") +
+    //                       " " +
+    //                       localStorage.getItem("l_request_start_date");
+    //                     localStorage.setItem(
+    //                       "employee_id",
+    //                       response_.employee_id
+    //                     );
+    //                     console.log(localStorage.getItem("employee_id"));
+    //                   });
+    //               } else {
+    //                 localStorage.setItem(
+    //                   "employee_id",
+    //                   response.response[0].employee_id
+    //                 );
+    //                 this.userrequest_start_date =
+    //                   localStorage.getItem("f_request_start_date") +
+    //                   " " +
+    //                   localStorage.getItem("l_request_start_date");
+    //                 this.openId
+    //                   .getAllRequests(response.response[0].employee_id)
+    //                   .subscribe(data => {
+    //                     this.dataSource = new MatTableDataSource(data);
+    //                   });
+    //               }
+    //             });
+    //         });
+    //     });
+    // });
   }
 
   // displayedColumns: string[] = [
