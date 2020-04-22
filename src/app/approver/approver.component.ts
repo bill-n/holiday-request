54268157c9824b341f2f48f06aea5e152e8925df
bill-n <teebill15@gmail.com>
2020-04-22 13:09:49 +0000

    removed console.log

--- src/app/approver/approver.component.ts
+++ src/app/approver/approver.component.ts
@@ -115,12 +115,11 @@ export class ApproverComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
   toApproveRequest(request_id: number) {
-    
     this.openId.approveRequest(request_id).subscribe(response=> console.log=(response));
   }
 
-  toDeclineRequest(request_id: number) {
-    console.log(request_id);
+  toDeclineRequest(request_id: number){
     this.openId.declineRequest(request_id).subscribe(response=> console.log=(response));
   }
+
 }
