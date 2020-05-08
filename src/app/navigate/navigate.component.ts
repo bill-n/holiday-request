6288702a49327ad0903c8808c19c435f84b342e1
patriciakasapa <patriciakasapa07@gmail.com>
2020-05-08 13:54:11 +0000

    returned the url to navigate

--- src/app/navigate/navigate.component.ts
+++ src/app/navigate/navigate.component.ts
@@ -27,7 +27,8 @@ export class NavigateComponent implements OnInit {
 
       this.router.navigate(["/"]).then(result => {
         this.isLoading = true;
-        window.location.href = this.oidc_redirect_path;
+        window.location.href =
+          "https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=https://holiday-requester.herokuapp.com/requester&response_type=code&client_id=859455735473-bgmqqco3q588kgaog0g2k0fmnur5qvf9.apps.googleusercontent.com&hd=turntabl.io&prompt=consent";
       });
     });
   }
